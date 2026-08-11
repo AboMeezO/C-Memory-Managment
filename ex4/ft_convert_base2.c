511595e24ce9a647b2ec23d160c6285040a80a39
Motch <abomeezo2@gmail.com>
2026-08-11 16:40:08 +0300

    holly fuck forgot to track those files

---
+++ ex4/ft_convert_base2.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 14:45:13 by mohammah          #+#    #+#             */
+/*   Updated: 2026/08/11 16:28:14 by mohammah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*hexaToDecimal(char *number)
+{
+	char	result;
+	char	hex;
+	char	index;
+
+	hex = "0123456789abcdef";
+	index = 0;
+	while (number[index])
+	{
+		result[index] = hex[number[index] / 16];
+		index++;
+	}
+	return (result);
+}
